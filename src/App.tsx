40b6f8e563da11e7cd19cf4f0bffffe41c2d4117
gonzalo <gonzalo.invernizz@gmail.com>
2023-05-30 08:55:29 -0300

    se agrego input as textaera

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 import React, { useReducer, useRef } from 'react'
 import './App.css'
 import { useStore } from './hooks/store'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form } from 'react-bootstrap'
 import InterchangeIcon from './assets/InterchangeIcon'
 import { FromLanguage, Language } from './types'
 import { AUTO_LENGUAGE, SUPPORTED_LAGNUAGES } from './constants/constants'
@@ -34,6 +34,7 @@ const App = () => {
         <Col>
           <h2>From</h2>
           <SelectorLanguages language={fromLanguage} lenguagesList={fromLanguages} handleChangeLang={onHandleSelectFromLanguages} />
+          <Form.Control as='textarea' style={{ marginTop: '10px', height: '100px' }} type="text" name="fromText" />
         </Col>
         <Col>
           <InterchangeIcon onHandleInterchange={onHandleInterchange} />
@@ -41,6 +42,7 @@ const App = () => {
         <Col>
           <h3>to</h3>
           <SelectorLanguages language={toLanguage} lenguagesList={toLengages} handleChangeLang= {onHandleSelectToLanguages} />
+          <Form.Control as='textarea' style={{ marginTop: '10px', height: '100px' }} type="text" name="toText" />
         </Col>
       </Row>
     </Container>
